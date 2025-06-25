3c2e268a35b7215ba55eb6ad52739311a1bf8bb1
pernillasterner <psterner@hotmail.com>
2025-06-25 12:53:06 +0200

    wip

---
+++ src/pages/components/RoomDropdown.tsx
@@ -0,0 +1,7 @@
+export default function RoomDropdown() {
+  return (
+    <>
+      <h3>Mötesplats</h3>
+    </>
+  )
+}
--- src/pages/select-time.tsx
+++ src/pages/select-time.tsx
@@ -1,10 +1,13 @@
 import Heading from "./components/Heading";
 import Layout from "./components/Layout";
+import RoomDropdown from "./components/RoomDropdown";
 
 export default function SelectTime() {
   return (
     <Layout title="Välj en tid">
       <Heading size="md">Välj en tid</Heading>
+
+      <RoomDropdown />
     </Layout>
   )
 }
