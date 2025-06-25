4f75edd749cbbd2bd956685a187919f9a2970d2d
pernillasterner <psterner@hotmail.com>
2025-06-25 11:37:37 +0200

    Add basic styling to heading and button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,20 @@ import Layout from "./components/Layout";
 export default function Home() {
   return (
       <>
-        <Layout title="Boka ett möte - test">Hello World</Layout>
+        <Layout>
+          {/* skapa en komponent */}
+          <h1 className="text-7xl sm:text-[5rem] tracking-[0.03em] leading-[1]] mt-16">
+            Boka ett rum
+          </h1>
+
+          {/* 
+          skapa en komponent
+          länka till sidan "välj en tid" 
+          */}
+          <button className="w-full bg-black text-white py-4 rounded-2xl text-base font-medium mt-auto border-1 [border-color:var(--white-10)] hover:bg-neutral-800 transition-all duration-300 ease-in-out focus:outline focus:ring-2 focus:ring-black">
+            Boka
+          </button>
+        </Layout>
       </>
   );
 }
