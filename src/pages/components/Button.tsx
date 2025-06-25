62fa937fb2e739cfd0a83bf933370c1747b20e19
pernillasterner <psterner@hotmail.com>
2025-06-25 13:50:48 +0200

    Use clsx to merge tailwind classes

--- src/pages/components/Button.tsx
+++ src/pages/components/Button.tsx
@@ -1,19 +1,30 @@
+import { cn } from "@/utils/cn";
+
 type ButtonProps = {
   children: React.ReactNode; // tar emot allt i react (strängar, andra komponenter)
   ariaLabel?: string
   onClick?: () => void // tar emot en funktion så fort event triggas
+  className?: string
 }
 
-export default function Button({ children, ariaLabel, onClick } : ButtonProps) {
+export default function Button({
+  children,
+  ariaLabel,
+  onClick,
+  className
+}: ButtonProps) {
   return (
     <button
       type="button"
       role="button"
       aria-label={ariaLabel}
       onClick={onClick}
-      className="w-full [background-color:var(--gray-800)] text-white py-4 rounded-2xl text-base font-medium mt-auto border-1 [border-color:var(--white-10)] hover:bg-neutral-800 transition-all duration-300 ease-in-out focus:outline focus:ring-2 focus:ring-black"
+      className={cn(
+        "w-full bg-[var(--gray-800)] text-white py-4 rounded-2xl text-base font-medium mt-auto border [border-color:var(--white-10)] hover:bg-neutral-800 transition-all duration-300 ease-in-out focus:outline focus:ring-2 focus:ring-black",
+        className
+      )}
     >
       {children}
     </button>
-  )
+  );
 }
--- src/pages/components/RoomDropdown.tsx
+++ src/pages/components/RoomDropdown.tsx
@@ -1,7 +1,14 @@
+import Button from "./Button";
+
 export default function RoomDropdown() {
   return (
-    <>
-      <h3>Mötesplats</h3>
-    </>
+    <div className="relative w-full max-w-xs my-4">
+      <h3 className="text-[color:var(--gray-850)]">Mötesplats</h3>
+
+      <div className="flex justify-between gap-2 px-4 pt-2 border-t border-gray-200">
+        <Button>Välj</Button>
+        <Button className="bg-[var(--gray-600)]">Avmarkera</Button>
+      </div>
+    </div>
   )
 }
