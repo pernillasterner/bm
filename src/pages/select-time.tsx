3c31e385b84c61fec261c90da6d494cdaa5da466
pernillasterner <psterner@hotmail.com>
2025-06-25 12:03:33 +0200

    Add select-time page

---
+++ src/pages/select-time.tsx
@@ -0,0 +1,10 @@
+import Heading from "./components/Heading";
+import Layout from "./components/Layout";
+
+export default function SelectTime() {
+  return (
+    <Layout title="Välj en tid">
+      <Heading>Välj en tid</Heading>
+    </Layout>
+  )
+}
