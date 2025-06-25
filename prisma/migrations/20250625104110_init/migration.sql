b73763e73c64275027cc646057783c2b3051415e
pernillasterner <psterner@hotmail.com>
2025-06-25 12:50:51 +0200

    Install and initialize prisma. Create tables for meetingrooms and migrate db

---
+++ prisma/migrations/20250625104110_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "MeetingRoom" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "capacity" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
